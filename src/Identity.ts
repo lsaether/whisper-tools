400d2fc2301a1c4949708f2dc6406aa3ca5d675f
lsaether <Lsaether@protonmail.com>
2018-07-05 13:34:03 +0300

    wip

--- src/Identity.ts
+++ src/Identity.ts
@@ -1,6 +1,15 @@
 import WhisperApi from './WhisperApi';
 
-class Identity {
-  
+import { KeyId } from './Types';
 
+class Identity {
+  public keyId: KeyId;
+  public shh: WhisperApi;
+  
+  constructor(keyId: KeyId, shh: WhisperApi) {
+    this.keyId = keyId;
+    this.shh = shh;
+  }
+
+  
 }
