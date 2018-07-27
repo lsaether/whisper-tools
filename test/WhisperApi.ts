c3a5df1b157aac7f9c05c61d497fe8e1bc36a5a2
lsaether <Lsaether@protonmail.com>
2018-07-27 23:46:53 +0300

    tslint

--- test/WhisperApi.ts
+++ test/WhisperApi.ts
@@ -3,46 +3,48 @@
  * flags. `geth --shh --rpc --nodiscover console`
  */
 
-import { expect } from 'chai';
+import { expect } from "chai";
 
-import WhisperApi from '../src/WhisperApi';
+import WhisperApi from "../src/WhisperApi";
 
-const LOCAL_PROVIDER = 'http://localhost:8545';
+const LOCAL_PROVIDER = "http://localhost:8545";
 
-describe('WhisperApi', () => {
+describe("WhisperApi", () => {
 
   let shh;
 
   before(() => {
     shh = new WhisperApi(LOCAL_PROVIDER);
-  })
+  });
 
-  it('gets the info and checks fields exist', async () => {
+  it("gets the info and checks fields exist", async () => {
     const info = await shh.info();
+    /* tslint:disable:no-unused-expression */
     expect(info.memory).to.exist;
     expect(info.messages).to.exist;
     expect(info.minPow).to.exist;
     expect(info.maxMessageSize).to.exist;
-  })
+    /* tslint:enable */
+  });
 
-  it('gets the version', async () => {
+  it("gets the version", async () => {
     const version = await shh.version();
-    expect(version).to.equal('6.0');
-  })
+    expect(version).to.equal("6.0");
+  });
 
-  it('generates a new keypair', async () => {
+  it("generates a new keypair", async () => {
     const keypairId = await shh.newKeyPair();
     expect(keypairId.length).to.equal(64);
-  })
+  });
 
-  it('gets a privatekey from keypair', async () => {
+  it("gets a privatekey from keypair", async () => {
     const keypairId = await shh.newKeyPair();
     const privkey = await shh.getPrivateKey(keypairId);
-    expect(privkey.slice(0,2)).to.equal('0x');
+    expect(privkey.slice(0, 2)).to.equal("0x");
     expect(privkey.length).to.equal(66);
-  })
+  });
 
-  it('gets a keypair from privkey', async () => {
+  it("gets a keypair from privkey", async () => {
     const keypairId = await shh.newKeyPair();
     const privkey = await shh.getPrivateKey(keypairId);
     const keypairIdMatch = await shh.addPrivateKey(privkey);
@@ -50,10 +52,8 @@ describe('WhisperApi', () => {
     // console.log(keypairId, keypairIdMatch);
     const privkeyMatch = await shh.getPrivateKey(keypairIdMatch);
     expect(privkey).to.equal(privkeyMatch);
-  })
+  });
 
-  it('')
+  it("");
 
-
-
-})
+});
